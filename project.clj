2372f5b723bcac5a9fc3a7bec543897e48c7e0bb
Vasilis Margioulas <vmargioulas@gmail.com>
2011-05-12 16:52:11 +0300

    testing moustache

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject ae-test "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [enlive "1.0.0"]
+		 [net.cgrand/moustache "1.0.0"]]
   :dev-dependencies [[appengine-magic "0.4.1"]])
--- src/ae_test/core.clj
+++ src/ae_test/core.clj
@@ -1,11 +1,24 @@
 (ns ae-test.core
-  (:require [appengine-magic.core :as ae]))
+  (:require [appengine-magic.core :as ae])
+  (:use [net.cgrand.moustache :only [app]]))
 
 
 (defn ae-test-app-handler [request]
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body "Hello, world!!!"})
+
+(def ae-test-app-handler
+     (app
+      [""]     {:status 200
+		:headers {"Content-Type" "text/plain"}
+		:body "Hello, world!!!"}
+      ["test"] {:status 200
+		:headers {"Content-Type" "text/plain"}
+		:body "Testing..."}
+      [&]      {:status 404
+		:body "Page Not Found"}))
+
 
 
 (ae/def-appengine-app ae-test-app #'ae-test-app-handler)
